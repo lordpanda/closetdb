a4dd54c3d8838d28e1249dd1ee98a6f17e5edc9d
Panda Cho <jediknightpanda@gmail.com>
2022-10-06 16:25:55 +0900

    node

---
+++ views/script.js
@@ -0,0 +1,17 @@
+function displayRecentlyAdded() {
+    
+    var grid = document.querySelector(".grid_container"); 
+    for (var i = 0; i < 7; i++) {
+        const item = document.createElement('div');
+        item.className = 'grid_item';
+        item.innerHTML = `<a href="#"><img src="./src/db/${i}.jpg"></a>`;
+        grid.appendChild(item);
+    }
+}
+
+function displayFilterCategory() {
+    var grid = document.querySelector(".grid_container"); 
+    for (var i = 0; i < 9; i++) {
+
+    }
+}
